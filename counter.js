90b47d9f4dc47fcf8da94add22843ccbdad96d4d
Khadijah-Abdulqadir03 <abdulqadirkhadijah25@gmail.com>
2026-08-13 14:29:37 +0100

    A passenger counter that counts the number of people entering.

---
+++ counter.js
@@ -0,0 +1,23 @@
+let count = 0
+let countEl = document.getElementById("count-el");
+
+function increment(){
+
+    count += 1;
+    countEl.textContent = count;
+
+}
+
+
+
+let saveEl = document.getElementById("save-el");
+function save(){
+
+    let opTor = count + " - ";
+    saveEl.textContent += opTor;
+    countEl.textContent = 0;
+    count = 0;
+    
+}
+
+
